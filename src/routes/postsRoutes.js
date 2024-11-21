541c514810c77aa10a45203f2eca22b385403ca5
josiasdev <josiasmartins098@gmail.com>
2024-11-21 11:18:54 -0300

    feat: comentei a parte do windows estou usando distribuição Linux o Arch Linux

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,5 +1,21 @@
 import express from "express"
-import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+// Windows
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'uploads/');
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, file.originalname);
+//     }
+// })
+// const upload = multer({ dest: "./uploads", storage});
+
+// Linux
+const upload = multer({dest: "./uploads"});
+
 
 const routes = (app) => {
     // criar(post), ler(get), atualizar(put), deletar(delete);
@@ -9,6 +25,7 @@ const routes = (app) => {
     app.get("/posts", listarPosts);
     // Rota para criar um post
     app.post("/posts", postarNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImagem);
 }
 
 export default routes;
