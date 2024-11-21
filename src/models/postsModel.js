924638b191b8708334d3b1c474d394d3b26597d3
josiasdev <josiasmartins098@gmail.com>
2024-11-21 11:01:14 -0300

    feat: realizo a conexão com o banco de dados e passo a coleção posts e na coleção insiro um post

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,8 +1,15 @@
 import conectarAoBanco from "../config/dbConfig.js";
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
 
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
     const db = conexao.db("imersao-instabytes");
     const colecao = db.collection("posts");
     return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    // inserir um post
+    return colecao.insertOne(novoPost);
 }
