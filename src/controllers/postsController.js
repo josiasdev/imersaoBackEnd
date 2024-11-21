890d5188977f50dbffdf2c33fdc3f5092235917e
josiasdev <josiasmartins098@gmail.com>
2024-11-21 11:00:08 -0300

    feat: criei uma função para postar novo post

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,20 @@
-import getTodosPosts from "../models/postsModel.js";
+import {getTodosPosts, criarPost} from "../models/postsModel.js";
 
 export async function listarPosts(request, response) {
     const resultadoPosts = await getTodosPosts();
     response.status(200).json(resultadoPosts);
+}
+
+export async function postarNovoPost(request, response) {
+    const novoPost = request.body;
+    // tentar
+    try {
+        const postCriado = await criarPost(novoPost);
+        response.status(200).json(postCriado);
+    }
+    // pegar erro
+    catch (erro) {
+        console.error("Erro:" + erro.message);
+        response.status(500).json({ "Erro": "Falha na requisição" });
+    }
 }
