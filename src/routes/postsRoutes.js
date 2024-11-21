3eb788a9b1be23f08c217c43a3b2951c0553fa45
josiasdev <josiasmartins098@gmail.com>
2024-11-21 10:50:55 -0300

    feat: criei uma rota para postar um novo post

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,14 @@
 import express from "express"
-import { listarPosts } from "../controllers/postsController.js";
+import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
 
 const routes = (app) => {
+    // criar(post), ler(get), atualizar(put), deletar(delete);
     app.use(express.json());
+
+    // Rota para buscar todos os posts
     app.get("/posts", listarPosts);
+    // Rota para criar um post
+    app.post("/posts", postarNovoPost);
 }
 
 export default routes;
