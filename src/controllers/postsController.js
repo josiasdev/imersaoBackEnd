cecfb87b82fb57bfed7c0778d75f2c7fb204b6d5
josiasdev <josiasmartins098@gmail.com>
2024-11-22 14:56:29 -0300

    feat: gerando uma descrição usando o serviço do google gemini.

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,7 @@
+import { getTodosPosts, criarPost, atualizarPost } from "../models/postsModel.js";
 import fs from "fs";
-import {getTodosPosts, criarPost, atualizarPost} from "../models/postsModel.js";
+import gerarDescricaoComGemini from "../services/geminiService.js";
+
 
 export async function listarPosts(request, response) {
     const resultadoPosts = await getTodosPosts();
@@ -33,19 +35,21 @@ export async function uploadImagem(request, response) {
         response.status(200).json(postCriado);
     } catch (erro) {
         console.error(erro.message);
-        response.status(500).json({"Erro": "Falha na requisição"});
+        response.status(500).json({ "Erro": "Falha na requisição" });
     }
 }
 
 export async function atualizarNovoPost(request, response) {
     const id = request.params.id;
     const urlImagem = `http://localhost:3000/${id}.png`;
-    const post = {
-        imgUrl: urlImagem,
-        descricao: request.body.descricao,
-        alt: request.body.alt
-    }
     try {
+        const imgBuffer = fs.readFileSync(`uploads/${id}.png`)
+        const descricao = await gerarDescricaoComGemini(imgBuffer);
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: request.body.alt
+        }
         const postCriado = await atualizarPost(id, post);
         response.status(200).json(postCriado);
     }
