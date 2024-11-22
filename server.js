15d99d5ef20f3237a362a80ca4f5e2c4fee1c9f7
josiasdev <josiasmartins098@gmail.com>
2024-11-22 14:03:25 -0300

    feat: servir arquivos estáticos de uma pasta upload

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
+
+// servir arquivos estáticos
+app.use(express.static("uploads"));
 routes(app);
 
 app.listen(3000, () => {
