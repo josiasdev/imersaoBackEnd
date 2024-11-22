8ae4a0bb741b7c517712552f7f513a579ad2edbf
josiasdev <josiasmartins098@gmail.com>
2024-11-22 13:19:06 -0300

    feat: função para enviar imagem

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import {getTodosPosts, criarPost} from "../models/postsModel.js";
 
 export async function listarPosts(request, response) {
@@ -16,5 +17,22 @@ export async function postarNovoPost(request, response) {
     catch (erro) {
         console.error("Erro:" + erro.message);
         response.status(500).json({ "Erro": "Falha na requisição" });
+    }
+}
+
+export async function uploadImagem(request, response) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: request.file.originalname,
+        alt: ""
+    };
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+        fs.renameSync(request.file.path, imagemAtualizada);
+        response.status(200).json(postCriado);
+    } catch (erro) {
+        console.error(erro.message);
+        response.status(500).json({"Erro": "Falha na requisição"});
     }
 }
